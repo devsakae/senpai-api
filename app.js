d6f567689364a262e8bc9b6562065f900b7b0c54
Rodrigo Sakae <devsakae@gmail.com>
2025-01-24 22:36:42 -0300

    minor fixes

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ app.use(express.json());
 
 const port = process.env.port || 3000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log("API starting at port", port);
 });
 
 app.get('/status', (_, res) => { 
-  return res.send({ 'status': 'funcional' });
+  return res.send({ "status": "Funcional" });
 })
 
 app.post('/msg', (req, res) => {
-  console.log(req);
-  return;
+  console.log(req.body);
+  return res.send({ "message": "Mensagem recebida" })
 })
