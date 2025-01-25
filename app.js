853703ce8b10d95ab176d2a23216e818319e4ea9
Rodrigo Sakae <devsakae@gmail.com>
2025-01-25 10:52:22 -0300

    refactor: api for wb

--- app.js
+++ app.js
@@ -2,28 +2,79 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const port = process.env.port || 3000;
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT } = process.env;
 
-app.listen(port, () => {
-  console.log("API starting at port", port);
+app.listen(PORT, () => {
+  console.log("API iniciou na porta", PORT);
 });
 
-app.get('/status', (_, res) => { 
-  return res.send({ "status": "Funcional" });
-})
+app.post("/webhook", async (req, res) => {
+  // log incoming messages
+  console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
 
-app.get('/teste', (req, res) => {
-  console.log(req.query)
-  console.log(req.query["hub.mode"])
-  console.log(req.query["hub.verify_token"])
-  return res.send({ 'message': 'test successfull' })
-})
+  // check if the webhook request contains a message
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
 
-app.get('/webhook', (req, res) => {
-  const queries = req.query;
-  if (queries["hub.verify_token"] !== process.env.VERIFY_TOKEN) return res.status(401).send({ "error": "token mismatch" });
-  if (queries["hub.mode"] === "subscribe") {
-    return res.send(queries["hub.challenge"]);
+  // check if the incoming message contains text
+  if (message?.type === "text") {
+    // extract the business number to send the reply from it
+    const business_phone_number_id =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        to: message.from,
+        text: { body: "Echo: " + message.text.body },
+        context: {
+          message_id: message.id, // shows the message as a reply to the original user message
+        },
+      },
+    });
+
+    // mark incoming message as read
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v21.0/${business_phone_number_id}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        status: "read",
+        message_id: message.id,
+      },
+    });
   }
-  return res.send({ "message": "no data?" });
+
+  res.sendStatus(200);
+});
+
+// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    // respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here. Checkout README.md to start.</pre>`);
 });
