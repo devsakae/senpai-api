33f4979fa441a66d2f458c6629d4d1b71c317555
Rodrigo Sakae <devsakae@gmail.com>
2025-02-02 22:55:36 -0300

    refator: template

--- src/templates/index.js
+++ src/templates/index.js
@@ -3,8 +3,47 @@ const { GRAPH_API_TOKEN, VERSION } = process.env;
 
 const greetFirstUser = async (data) => {
   const metadata = data.body.entry?.[0].changes?.[0].value?.metadata;
-  console.info('greeting first user', metadata);
-  try {
+  console.info('greeting first user with phone', metadata?.display_phone_number);
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', `Bearer ${GRAPH_API_TOKEN}`);
+
+  const raw = JSON.stringify({
+    messaging_product: 'whatsapp',
+    recipient_type: 'individual',
+    to: metadata?.display_phone_number,
+    type: 'template',
+    template: {
+      name: 'modo_manutencao',
+      language: {
+        code: 'pt_br',
+      },
+      components: [
+        {
+          type: 'header',
+          parameters: [],
+        },
+      ],
+    },
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  fetch(
+    `https://graph.facebook.com/${VERSION}/${metadata?.phone_number_id}/messages`,
+    requestOptions,
+  )
+    .then((response) => response.text())
+    .then((result) => console.log("sent!", result))
+    .catch((error) => console.error("error!", error));
+
+  /*   try {
     await axios({
       method: 'POST',
       url: `https://graph.facebook.com/${VERSION}/${metadata?.phone_number_id}/messages`,
@@ -32,8 +71,8 @@ const greetFirstUser = async (data) => {
     });
   } catch (err) {
     console.error('Erro ao enviar mensagem de boas vindas!');
-    console.error(err);
-  }
+    console.error(err.code);
+  } */
 };
 
 module.exports = {
