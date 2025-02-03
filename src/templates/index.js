8decddf405678a12b5c88ea2105f310a2da68a97
Rodrigo Sakae <devsakae@gmail.com>
2025-02-02 22:38:45 -0300

    feat: template

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,37 +1,39 @@
-const axios = require("axios");
+const axios = require('axios');
 const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT, VERSION } = process.env;
 
-const greetFirstUser = async (data, res) => {
-  const business_phone_number_id = data.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
-  const payload = req.body.entry?.[0]?.changes;
-  const message = payload[0]?.value?.messages?.[0];
-
+const greetFirstUser = async (data) => {
+  const metadata = data.body.entry?.[0].changes?.[0].value?.metadata;
   try {
     await axios({
-      method: "POST",
-      url: `https://graph.facebook.com/${VERSION}/${business_phone_number_id}/messages`,
+      method: 'POST',
+      url: `https://graph.facebook.com/${VERSION}/${metadata?.phone_number_id}/messages`,
       headers: {
         Authorization: `Bearer ${GRAPH_API_TOKEN}`,
       },
       data: {
-        messaging_product: "whatsapp",
-        recipient_type: "individual",
-        to: message.from,
-        type: "text",
-        text: {
-          preview_url: false,
-          body: "Bem vindo ao Senpai Bot. Estamos em construção para melhor atender!"
-        }
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: metadata?.display_phone_number,
+        type: 'template',
+        template: {
+          name: 'modo_manutencao',
+          language: {
+            code: 'pt_br',
+          },
+          components: [
+            {
+              type: 'header',
+              parameters: [],
+            },
+          ],
+        },
       },
     });
   } catch (err) {
-    console.error("Erro ao enviar mensagem de boas vindas!")
-    console.error(err);
-  } finally {
-    return res.sendStatus(200);
+    console.error('Erro ao enviar mensagem de boas vindas!');
   }
 };
 
 module.exports = {
   greetFirstUser,
-}
+};
