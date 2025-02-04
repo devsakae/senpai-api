e614225a8482022c97a2234366a16ef7e57eaa87
Rodrigo Sakae <devsakae@gmail.com>
2025-02-03 21:51:15 -0300

    complete remodeled

--- app.js
+++ app.js
@@ -1,68 +1,117 @@
-const express = require('express');
-const axios = require('axios');
-const { greetFirstUser } = require('./src/templates');
 
-const app = express();
-app.use(express.json());
+const https = require("https");
 
-const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT, VERSION } = process.env;
+exports.handler = async (event)  => {
+  const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
+  const WHATSAPP_TOKEN = process.env.GRAPH_API_TOKEN;
 
-app.listen(PORT, () => {
-  console.log("API iniciou na porta", PORT);
-});
-
-app.get("/", (_req, res) => res.send({ "status": "online" }) );
-app.get("/healthcheck", (req, res) => res.status(200).end());
-app.get("/coffee", (_req, res) => res.status(418).end());
-
-app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  // check the mode and token sent are correct
-  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
-    console.log("Webhook verified successfully with challenge", challenge);
-    return res.status(200).send(challenge);
-  } else {
-    return res.sendStatus(403).end();
-  }
-});
-
-app.post("/webhook", async (req, res) => {
-  
-  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-  // const payload = req.body.entry[0]?.changes[0]?.value;
-  
-  // if (payload?.messages[0]?.type === "request_welcome") return greetFirstUser(req, res);
-  
-  if (req.body.entry[0]?.changes[0]?.value?.messages && req.body.entry[0]?.changes[0]?.value?.messages[0]?.type === "text") {
-    const payload = req.body.entry[0]?.changes[0]?.value;
-    const { metadata, contacts } = payload;
-    const message_content = payload?.messages[0]?.type === "text" ? payload?.messages[0]?.text?.body : payload?.messages[0]?.type || "unknown";
-    console.info("msg from", contacts[0]?.profile?.name, metadata?.display_phone_number, ">", message_content, "[" + payload?.messages[0]?.type + "]");
-    const business_phone_number_id = metadata?.phone_number_id;
-    try {
-      await axios({
-        method: "POST",
-        url: `https://graph.facebook.com/${VERSION}/${business_phone_number_id}/messages`,
-        headers: {
-          Authorization: `Bearer ${GRAPH_API_TOKEN}`,
-        },
-        data: {
-          messaging_product: "whatsapp",
-          status: "read",
-          message_id: payload?.messages[0]?.id
-        },
-      });
-    } catch(err) {
-      console.error("ERROR:", err);
-    } finally {
-      if (metadata?.display_phone_number.startsWith("55")) {
-        await greetFirstUser(req);
+  let response;
+  if (event?.requestContext?.http?.method === "GET") {
+    // https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+    // to learn more about GET request for webhook verification
+    let queryParams = event?.queryStringParameters;
+    if (queryParams != null) {
+      const mode = queryParams["hub.mode"];
+      if (mode == "subscribe") {
+        const verifyToken = queryParams["hub.verify_token"];
+        if (verifyToken == VERIFY_TOKEN) {
+          let challenge = queryParams["hub.challenge"];
+          response = {
+              "statusCode": 200,
+              "body": parseInt(challenge),
+              "isBase64Encoded": false
+          };
+        } else {
+          const responseBody = "Error, wrong validation token";
+          response = {
+              "statusCode": 403,
+              "body": JSON.stringify(responseBody),
+              "isBase64Encoded": false
+          };
+        }
+      } else {
+          const responseBody = "Error, wrong mode";
+          response = {
+              "statusCode": 403,
+              "body": JSON.stringify(responseBody),
+              "isBase64Encoded": false
+        };
       }
     }
+    else {
+      const responseBody = "Error, no query parameters";
+      response = {
+          "statusCode": 403,
+          "body": JSON.stringify(responseBody),
+          "isBase64Encoded": false
+      };
+    }
+  } else if (event?.requestContext?.http?.method === "POST") {
+    // process POST request (WhatsApp chat messages)
+    // https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    // to learn about WhatsApp text message payload structure
+    let body = JSON.parse(event.body)
+    let entries = body.entry;
+    for (let entry of entries) {
+      for (let change of entry.changes) {
+        let value = change.value;
+        if(value != null) {
+          let phone_number_id = value.metadata.phone_number_id;
+          if (value.messages != null) {
+            for (let message of value.messages) {
+              if (message.type === 'text') {
+                let from = message.from;
+                let message_body = message.text.body;
+                let reply_message = "Ack from AWS lambda: " + message_body;
+                sendReply(phone_number_id, WHATSAPP_TOKEN, from, reply_message);
+                const responseBody = "Done";
+                response = {
+                    "statusCode": 200,
+                    "body": JSON.stringify(responseBody),
+                    "isBase64Encoded": false
+                };
+              }
+            }
+          }
+        }
+      }
+    }
+  } else {
+    const responseBody = "Unsupported method";
+    response = {
+        "statusCode": 403,
+        "body": JSON.stringify(responseBody),
+        "isBase64Encoded": false
+    };
   }
-  console.info("***\n", req?.body?.entry[0]?.changes[0]?.value);
-  return res.sendStatus(200);
-});
+  
+  return response;
+}
+
+const sendReply = (phone_number_id, whatsapp_token, to, reply_message) => {
+  let json = {
+    messaging_product: "whatsapp",
+    to: to,
+    text: { body: reply_message },
+  };
+  let data = JSON.stringify(json);
+  let path = "/v22.0/"+phone_number_id+"/messages?access_token="+whatsapp_token;
+  let options = {
+    host: "graph.facebook.com",
+    path: path,
+    method: "POST",
+    headers: { "Content-Type": "application/json" }
+  };
+  let callback = (response) => {
+    let str = "";
+    response.on("data", (chunk) => {
+      str += chunk;
+    });
+    response.on("end", () => {
+    });
+  };
+  let req = https.request(options, callback);
+  req.on("error", (e) => {});
+  req.write(data);
+  req.end();
+}
---
+++ app2.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const axios = require('axios');
+const { greetFirstUser } = require('./src/templates');
+
+const app = express();
+app.use(express.json());
+
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT, VERSION } = process.env;
+
+app.listen(PORT, () => {
+  console.log("API iniciou na porta", PORT);
+});
+
+app.get("/", (_req, res) => res.send({ "status": "online" }) );
+app.get("/healthcheck", (req, res) => res.status(200).end());
+app.get("/coffee", (_req, res) => res.status(418).end());
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    console.log("Webhook verified successfully with challenge", challenge);
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403).end();
+  }
+});
+
+app.post("/webhook", async (req, res) => {
+  
+  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  // const payload = req.body.entry[0]?.changes[0]?.value;
+  
+  // if (payload?.messages[0]?.type === "request_welcome") return greetFirstUser(req, res);
+  
+  if (req.body.entry[0]?.changes[0]?.value?.messages && req.body.entry[0]?.changes[0]?.value?.messages[0]?.type === "text") {
+    const payload = req.body.entry[0]?.changes[0]?.value;
+    const { metadata, contacts } = payload;
+    const message_content = payload?.messages[0]?.type === "text" ? payload?.messages[0]?.text?.body : payload?.messages[0]?.type || "unknown";
+    console.info("msg from", contacts[0]?.profile?.name, metadata?.display_phone_number, ">", message_content, "[" + payload?.messages[0]?.type + "]");
+    const business_phone_number_id = metadata?.phone_number_id;
+    try {
+      await axios({
+        method: "POST",
+        url: `https://graph.facebook.com/${VERSION}/${business_phone_number_id}/messages`,
+        headers: {
+          Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          status: "read",
+          message_id: payload?.messages[0]?.id
+        },
+      });
+    } catch(err) {
+      console.error("ERROR:", err);
+    } finally {
+      if (metadata?.display_phone_number.startsWith("55")) {
+        await greetFirstUser(req);
+      }
+    }
+  }
+  console.info("***\n", req?.body?.entry[0]?.changes[0]?.value);
+  return res.sendStatus(200);
+});
