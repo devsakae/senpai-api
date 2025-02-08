bd6ac6e81f7c72ec5bf743d5c946b97d7ace87e3
Rodrigo Sakae <devsakae@gmail.com>
2025-02-08 11:43:33 -0300

    last try

--- src/utils/sender.js
+++ src/utils/sender.js
@@ -1,17 +1,22 @@
+const { VERSION, GRAPH_API_TOKEN, PHONE_NUMBER_ID } = process.env;
 const { default: axios } = require('axios');
-const { jsonHeaders } = require("./index");
+const { jsonHeaders } = require('./index');
 
 const dispatchAxios = async (data) => {
   console.log('dispatch 2', jsonHeaders);
   return await axios({
-    jsonHeaders,
+    method: 'POST',
+    url: `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`,
+    headers: {
+      Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
     data: data,
   })
     .then((response) => console.log('dispatch/ok', response))
     .catch((error) => console.error('dispatch/error', error));
 };
 
-
 module.exports = {
   dispatchAxios,
-}
+};
