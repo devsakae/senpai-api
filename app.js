a3eca41c63030b7799c1190fa6105618877b4382
Rodrigo Sakae <devsakae@gmail.com>
2025-02-10 21:51:04 -0300

    mark as read must be text

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ const { WEBHOOK_VERIFY_TOKEN, PORT } = process.env;
           message_content,
           '[' + payload?.messages[0]?.type + ']',
         );
+        await markAsRead(req.body.entry[0]?.changes[0]?.value);
       }
-      await markAsRead(req.body.entry[0]?.changes[0]?.value);
       await checkContact(req);
       return res.sendStatus(200);
     });
