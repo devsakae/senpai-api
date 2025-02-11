c950ba5928177343a28d62c63956b6e2ab2195c4
Rodrigo Sakae <devsakae@gmail.com>
2025-02-10 21:38:14 -0300

    importing env

--- src/templates/sticker.js
+++ src/templates/sticker.js
@@ -1,5 +1,6 @@
 // const { Sticker, createSticker, StickerTypes } = require('@laxeder/wa-sticker');
 const { default: axios } = require("axios");
+const { VERSION, GRAPH_API_TOKEN, PHONE_NUMBER_ID } = process.env;
 
 const stickerTutorial = async (req) => {
   const payload = req.body.entry[0]?.changes[0]?.value;
