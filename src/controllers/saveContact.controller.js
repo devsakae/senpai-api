5a26fcdb1a308ad259fe7384d619bd39c72139cd
Rodrigo Sakae <devsakae@gmail.com>
2025-02-13 23:48:17 -0300

    e1100 mongodb error

--- src/controllers/saveContact.controller.js
+++ src/controllers/saveContact.controller.js
@@ -1,45 +1,52 @@
 const { senpaiMongoDb } = require('../utils/connections');
 const { message_hello } = require('../templates');
-const {
-  checkCommand,
-} = require('./checkCommand.controller');
+const { checkCommand } = require('./checkCommand.controller');
 
 const checkContact = async (req) => {
   const contact = req.body.entry[0]?.changes[0]?.value?.contacts[0];
-  const sender = await senpaiMongoDb
-    .collection('customers')
-    .findOneAndUpdate(
-      { wa_id: contact.wa_id },
-      { $set: { last_contact: new Date() } },
-      { upsert: true },
-    );
-
-  if (!sender) {
-    console.info('primeiro contato do usuário!');
-    await senpaiMongoDb
-      .collection('customers')
-      .insertOne({
-        wa_id: contact.wa_id,
+  const sender = await senpaiMongoDb.collection('customers').findOneAndUpdate(
+    { wa_id: contact.wa_id },
+    {
+      $set: {
         name: contact?.profile?.name,
         contact: contact,
-        first_contact: new Date(),
         last_contact: new Date(),
-      })
-      .then((response) => {
-        console.log('usuário criado no MongoDB?', response.data);
-        console.log('response:', response)
-        // if (!response.data.acknowledged)
-        //   throw new Error({
-        //     message: 'Erro criando usuário no MongoDB',
-        //     code: 500,
-        //   });
-      })
-      .catch((err) => console.error('erro:', err))
-      .finally(() => {
-        console.log('enviando hello...');
-        return message_hello(req);
-      });
-  }
+      },
+    },
+    { upsert: true },
+  );
+
+  console.log(sender);
+  return;
+  // return await message_hello(req);
+
+
+  // if (!sender) {
+  //   console.info('primeiro contato do usuário!');
+  //   await senpaiMongoDb
+  //     .collection('customers')
+  //     .insertOne({
+  //       wa_id: contact.wa_id,
+  //       name: contact?.profile?.name,
+  //       contact: contact,
+  //       first_contact: new Date(),
+  //       last_contact: new Date(),
+  //     })
+  //     .then((response) => {
+  //       console.log('usuário criado no MongoDB?', response.data);
+  //       console.log('response:', response);
+  //       if (!response.data.acknowledged)
+  //         throw new Error({
+  //           message: 'Erro criando usuário no MongoDB',
+  //           code: 500,
+  //         });
+  //     })
+  //     .catch((err) => console.error('erro:', err))
+  //     .finally(() => {
+  //       console.log('enviando hello...');
+  //       return message_hello(req);
+  //     });
+  // }
 
   /* Testing for admin and subadmin */
   if (
@@ -47,7 +54,7 @@ const checkContact = async (req) => {
     contact.wa_id === process.env.BOT_SUBADMIN_WAID
   ) {
     // if (!checkCommand(req)) return await checkLastInteraction(sender, req);
-    return await checkCommand(sender, req)
+    return await checkCommand(sender, req);
   }
 
   // if (!checkCommand(req)) {
