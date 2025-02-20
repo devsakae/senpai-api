7307fb44f7a7da184b1416c1dc7e1d2d3f69556c
Rodrigo Sakae <devsakae@gmail.com>
2025-02-20 12:31:14 -0300

    fix: listening replies

--- src/controllers/checkCommand.controller.js
+++ src/controllers/checkCommand.controller.js
@@ -17,11 +17,11 @@ const checkLastInteraction = async (sender, req) => {
 const checkCommand = async (sender, req) => {
   const user_sent = req.body.entry[0]?.changes[0]?.value?.messages[0];
   if (user_sent?.type === 'text') {
-    if (user_sent?.text?.body === '.canal') return await canal(req);
-    if (user_sent?.text?.body === '.suporte') return await getSuporte(req);
-    if (user_sent?.text?.body === '.sobre') return await sobre(req);
-    if (user_sent?.text?.body === '.menu' || user_sent?.text?.body === '.m') return await rootMenu(req.body.entry[0]?.changes[0]?.value?.contacts[0]);
-    if (user_sent?.text?.body === '.sticker' || user_sent?.text?.body === '.s')
+    if (user_sent?.text?.body === '.canal' || user_sent?.interactive?.button_reply?.title === '.canal') return await canal(req);
+    if (user_sent?.text?.body === '.suporte' || user_sent?.interactive?.button_reply?.title === '.suporte') return await getSuporte(req);
+    if (user_sent?.text?.body === '.sobre' || user_sent?.interactive?.button_reply?.title === '.sobre') return await sobre(req);
+    if (user_sent?.text?.body === '.menu'  || user_sent?.text?.body === '.m' || user_sent?.interactive?.button_reply?.title === '.menu') return await rootMenu(req.body.entry[0]?.changes[0]?.value?.contacts[0]);
+    if (user_sent?.text?.body === '.sticker' || user_sent?.text?.body === '.s' || user_sent?.interactive?.button_reply?.title === '.sticker' )
       return await stickerTutorial(req);
   }
   if (user_sent?.type === 'image') {
