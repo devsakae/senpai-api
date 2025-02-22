07c6ee38d54d63b7af683315bf17ffd7c28c9cc0
Rodrigo Sakae <devsakae@gmail.com>
2025-02-22 07:57:21 -0300

    adjusting

--- src/assinaturas/index.js
+++ src/assinaturas/index.js
@@ -1,18 +1,26 @@
-const fs = require('fs');
 const { default: axios } = require('axios');
-const coupons = require('../../data/cp.json');
 const { senpaiMongoDb } = require('../utils/connections');
 const { randomizeThis, msg_premium_thankyou } = require('../templates/info');
+const { sendAdmin } = require('../utils/sender');
 const { VERSION, GRAPH_API_TOKEN, PHONE_NUMBER_ID } = process.env;
 
 const senpaiCoupons = async () => {
-  return await senpaiMongoDb.collection('coupons').find().toArray();
-}
+  const dbCoupons = await senpaiMongoDb.collection('coupons').find().toArray();
+  if (dbCoupons.filter((el) => el.left > 0).length === 0) return false;
+  return dbCoupons;
+};
 
 const checkCupom = async (body, user) => {
   const userCoupon = body.split(' ')[1].trim();
-  if (coupons[userCoupon] && coupons[userCoupon] > 0) {
-    return await senpaiMongoDb.collection('customers').findOneAndUpdate(
+  const dbCoupons = await senpaiCoupons();
+  const validCoupom = dbCoupons.find(
+    (el) => el.code === userCoupon && el.left > 0,
+  );
+
+  if (validCoupom) {
+    return await senpaiMongoDb
+      .collection('customers')
+      .findOneAndUpdate(
         { wa_id: user.wa_id },
         {
           $set: {
@@ -26,14 +34,17 @@ const checkCupom = async (body, user) => {
         { upsert: true },
       )
       .then(async (res) => {
-        coupons[userCoupon] = coupons[userCoupon] - 1;
-        fs.writeFileSync(
-          './data/cp.json',
-          JSON.stringify(coupons, null, 4),
-          'utf-8',
-          (err) => err,
-        );
-        console.info('🔆 Usuário', res?.name, 'virou premium com o cupom', userCoupon);
+        await senpaiMongoDb
+          .collection('coupons')
+          .findOneAndUpdate({ _id: validCoupom._id }, { $inc: { left: -1 } })
+          .then((cpres) => {
+            const newPremiumUser = `🔆 Usuário ${res?.name} @${res?.wa_id} virou premium com o cupom ${userCoupon}! Ainda restam: ${cpres.left}`;
+            console.info(newPremiumUser);
+          })
+          .catch((err) =>
+            console.error('Error updating coupom', err.response?.data || err),
+          )
+          .finally(async () => await sendAdmin(newPremiumUser));
         return await welcome_premium(res);
       })
       .catch((err) =>
@@ -52,17 +63,17 @@ const welcome_premium = async ({ wa_id }) => {
       'Content-Type': 'application/json',
     },
     data: {
-      messaging_product: "whatsapp",    
-      recipient_type: "individual",
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
       to: wa_id,
-      type: "text",
+      type: 'text',
       text: {
-          preview_url: false,
-          body: welcome_message
-      }
-    }
-  })
-}
+        preview_url: false,
+        body: welcome_message,
+      },
+    },
+  });
+};
 
 module.exports = {
   checkCupom,
--- src/utils/sender.js
+++ src/utils/sender.js
@@ -1,4 +1,5 @@
-const { VERSION, GRAPH_API_TOKEN, PHONE_NUMBER_ID } = process.env;
+const { VERSION, GRAPH_API_TOKEN, PHONE_NUMBER_ID, SUPORTE_TECNICO, BOT_ADMIN_WAID } =
+  process.env;
 const { default: axios } = require('axios');
 
 const dispatchAxios = async (data) => {
@@ -15,6 +16,32 @@ const dispatchAxios = async (data) => {
     .catch((error) => console.error('dispatch/error', error));
 };
 
+const sendAdmin = async (payload) => {
+  return await axios({
+    method: 'POST',
+    url: `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`,
+    headers: {
+      Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    data: {
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
+      to: SUPORTE_TECNICO,
+      type: 'text',
+      text: {
+        preview_url: false,
+        body: payload,
+      },
+    },
+  })
+    .then((response) => console.log('send admin/ok', response))
+    .catch((error) =>
+      console.error('send admin/error', error.response?.data || error),
+    );
+};
+
 module.exports = {
   dispatchAxios,
+  sendAdmin,
 };
