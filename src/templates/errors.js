f816e7ce83978005b2e7474a76261b0b9b73dfb6
Rodrigo Sakae <devsakae@gmail.com>
2025-02-22 08:15:42 -0300

    req refactor

--- src/templates/errors.js
+++ src/templates/errors.js
@@ -3,7 +3,7 @@ const { randomizeThis, msg_limitsticker } = require("./info");
 const { VERSION, PHONE_NUMBER_ID, GRAPH_API_TOKEN } = process.env;
 
 const limitedStickers = async (req) => {
-  const limit_message = randomizeThis(msg_limitsticker) + `\n\nVocê pode tentar novamente a partir de ${new Date((req.entry.changes[0]?.value?.messages[0]?.timestamp * 1000) + 86400).toString()}`
+  const limit_message = randomizeThis(msg_limitsticker) + `\n\nVocê pode tentar novamente a partir de ${new Date((req.body?.entry[0].changes[0]?.value?.messages[0]?.timestamp * 1000) + 86400).toString()}`
   return await axios({
     method: 'POST',
     url: `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`,
