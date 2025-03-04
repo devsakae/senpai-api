cf6780c1fa17cd65de2bba46016b76fe820cf9a9
Rodrigo Sakae <devsakae@gmail.com>
2025-03-03 22:01:42 -0300

    adjusting entries

--- src/controllers/flow.controller.js
+++ src/controllers/flow.controller.js
@@ -4,7 +4,7 @@ const getFeedbackResponse = async (req) => {
   const payload = JSON.parse(req.body.entry[0]?.changes[0]?.value?.messages[0]?.interactive?.nfm_reply?.response_json);
   const contact = req.body.entry[0]?.changes[0]?.value?.contacts[0];
   let response = 'Flow *' + payload?.flow_token + '* enviado por ' + contact.profile.name + ' (' + contact.wa_id + '):\n\n';
-  Object.entries(payload).forEach((k, v) => response = response + '\n- ' + k + ': ' + v);
+  Object.entries(payload).forEach((k) => response = response + '\n- ' + k[0] + ': ' + k[1]);
   console.log(response);
   await sendAdmin(response);
 }
