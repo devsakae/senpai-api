8fd726b7382861cf5870d6dec74fcb983d176410
Rodrigo Sakae <devsakae@gmail.com>
2025-03-10 07:45:20 -0300

    fix: test log

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -3,6 +3,7 @@ const { checkCommand } = require('./checkCommand.controller');
 const { manualPremiumActivation } = require('./premium.controller')
 
 const adminCommand = async (req) => {
+  console.log('startswith', ADMIN_CMD_ADDPREMIUM)
   const commands = req.body.entry[0]?.changes[0]?.value?.messages[0]?.text?.body
   if (commands.startsWith(ADMIN_CMD_ADDPREMIUM)) {
     return await manualPremiumActivation(req);
