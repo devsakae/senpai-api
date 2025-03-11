235ca2ce5c22ac2fd409ac1f279485dead725f97
Rodrigo Sakae <devsakae@gmail.com>
2025-03-11 20:47:19 -0300

    fix: quick fix

--- src/controllers/checkCommand.controller.js
+++ src/controllers/checkCommand.controller.js
@@ -29,7 +29,7 @@ const checkCommand = async (user, req) => {
   
   const today = new Date();
   const user_sent = req.body.entry[0]?.changes[0]?.value?.messages[0];
-  if (user.name.includes("Rodrigo Sakae")) console.log(payload);
+  if (user.name.includes("Rodrigo Sakae")) console.log(user_sent);
   
   if (user_sent?.type === 'button'  && user_sent?.button?.payload === 'Possuo um Código') {
     return await flow_premium_activation(req);
