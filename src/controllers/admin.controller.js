94e1ede00d7b92d9e4c03b8ffb068e7e6c143a08
Rodrigo Sakae <devsakae@gmail.com>
2025-03-10 21:55:06 -0300

    fix: mocking admin last time

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,13 +1,14 @@
 const { ADMIN_CMD_ADDPREMIUM } = process.env;
 const { checkCommand } = require('./checkCommand.controller');
 const { manualPremiumActivation } = require('./premium.controller')
+const adminDate = new Date(1,1,1);
 
 const adminCommand = async (req) => {
   const commands = req.body.entry[0]?.changes[0]?.value?.messages[0]?.text?.body || ''
   if (commands.startsWith(ADMIN_CMD_ADDPREMIUM)) {
     return await manualPremiumActivation(req);
   }
-  return await checkCommand({ premium: true }, req);
+  return await checkCommand({ premium: true, last_time: { contact: adminDate, image: adminDate, text: adminDate, video: adminDate } }, req);
 }
 
 module.exports = {
