788e78d0752bf59264d49d7d3222dd88002913b8
Rodrigo Sakae <devsakae@gmail.com>
2025-03-13 23:03:37 -0300

    fix: adding days, not years

--- src/controllers/premium.controller.js
+++ src/controllers/premium.controller.js
@@ -107,7 +107,7 @@ const manualPremiumActivation = async (req) => {
   if (commands.length !== 4 || (commands[2] !== 'premium' && commands[2] !== 'basico')) return sendAdmin('Erro: Utilize o comando', ADMIN_CMD_ADDPREMIUM, '[wa_id] [premium/basico] [dias], como no exemplo: ', ADMIN_CMD_ADDPREMIUM, '554899787078 basico 30')
   const today = new Date();
   const expirationDate = today;
-  expirationDate.setDate(today.getDate() + commands[3])
+  expirationDate.setDate(today.getDate() + Number(commands[3]))
   const newPremiumUser = await senpaiMongoDb
     .collection('customers')
     .findOneAndUpdate(
