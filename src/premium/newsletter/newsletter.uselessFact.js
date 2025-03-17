585da434511e5029846d8e9af8bfed2858509dcc
Rodrigo Sakae <devsakae@gmail.com>
2025-03-16 21:45:56 -0300

    fix: useless fact

--- src/premium/newsletter/newsletter.uselessFact.js
+++ src/premium/newsletter/newsletter.uselessFact.js
@@ -5,7 +5,9 @@ const getUselessFact = async () => {
   const uselessFact = await axios.request({
     method: 'GET',
     url: 'https://uselessfacts.jsph.pl/api/v2/facts/random',
-  });
+  }).then(res => res.data.text)
+    .catch(err => console.error(err.data || err));
+
   if (uselessFact.length > 0) {
     const uselessFactTranslated = await googleTranslate({ source: "en", target: "pt-BR", query: uselessFact })
     return "\n\n" + uselessFactTranslated;
